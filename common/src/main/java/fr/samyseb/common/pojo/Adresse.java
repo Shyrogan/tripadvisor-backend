446cee37fb4ccf4e798c4dd02f409c436735b9fc
VIAL Sébastien <sebvi66@hotmail.fr>
2023-11-21 11:38:13 +0100

    fix(project): java 11

--- common/src/main/java/fr/samyseb/common/pojo/Adresse.java
+++ common/src/main/java/fr/samyseb/common/pojo/Adresse.java
@@ -1,22 +1,45 @@
 package fr.samyseb.common.pojo;
 
-import io.javalin.validation.Validator;
 import org.jdbi.v3.core.mapper.reflect.ColumnName;
 
 import java.util.UUID;
-import java.util.function.Function;
 
-public record Adresse(
-        @ColumnName("id_adresse")
-        UUID id,
-        String numero,
-        String rue,
-        String ville,
-        String pays
-) {
-    public static Function<Validator<Adresse>, Validator<Adresse>> VALIDATOR = v -> v
-            .check(a -> a.numero().length() <= 4 && !a.numero().isBlank(), "Le numéro de l'adresse doit avoir une longueur comprise entre 1 et 4 caractères non vides.")
-            .check(a -> a.rue().length() <= 64 && a.rue().length() >= 6 && !a.rue().isBlank(), "La rue de l'adresse doit avoir une longueur comprise entre 6 et 64 caractères non vides.")
-            .check(a -> a.ville().length() <= 64 && a.ville().length() >= 6 && !a.ville().isBlank(), "La ville de l'adresse doit avoir une longueur comprise entre 6 et 64 caractères non vides.")
-            .check(a -> a.pays().length() <= 32 && a.pays().length() >= 6 && !a.pays().isBlank(), "Le pays de l'adresse doit avoir une longueur comprise entre 6 et 32 caractères non vides.");
+public class Adresse {
+    private final UUID id;
+    private final String numero;
+    private final String rue;
+    private final String ville;
+    private final String pays;
+
+    public Adresse(
+            @ColumnName("id_adresse") UUID id, String numero,
+            String rue, String ville, String pays
+    ) {
+        this.id = id;
+        this.numero = numero;
+        this.rue = rue;
+        this.ville = ville;
+        this.pays = pays;
+    }
+
+    public UUID id() {
+        return id;
+    }
+
+    public String numero() {
+        return numero;
+    }
+
+    public String rue() {
+        return rue;
+    }
+
+    public String ville() {
+        return ville;
+    }
+
+    public String pays() {
+        return pays;
+    }
+    
 }
--- common/src/main/java/fr/samyseb/common/pojo/Hotel.java
+++ common/src/main/java/fr/samyseb/common/pojo/Hotel.java
@@ -1,23 +1,43 @@
 package fr.samyseb.common.pojo;
 
-import io.javalin.validation.Validator;
 import org.jdbi.v3.core.mapper.Nested;
 import org.jdbi.v3.core.mapper.reflect.ColumnName;
 
 import java.util.UUID;
-import java.util.function.Function;
 
-public record Hotel(
-        @ColumnName("id_hotel")
-        UUID id,
-        String nom,
-        int etoiles,
-        @Nested
-        Adresse adresse,
-        String url
-) {
-    public static Function<Validator<Hotel>, Validator<Hotel>> VALIDATOR = v -> v
-            .check(h -> h.nom().length() <= 64 && h.nom().length() >= 6 && !h.nom().isBlank(), "La nom de l'hôtel doit avoir une longueur comprise entre 6 et 64 caractères non vides.")
-            .check(h -> h.etoiles() >= 1 && h.etoiles() <= 5, "Les étoiles de l'hôtel doivent-être comprisent entre 1 et 5");
+public class Hotel {
 
+    private final UUID id;
+    private final String nom;
+    private final int etoiles;
+    private final Adresse adresse;
+    private final String url;
+
+    public Hotel(@ColumnName("id_hotel") UUID id, String nom, int etoiles, @Nested Adresse adresse, String url) {
+        this.id = id;
+        this.nom = nom;
+        this.etoiles = etoiles;
+        this.adresse = adresse;
+        this.url = url;
+    }
+
+    public UUID id() {
+        return id;
+    }
+
+    public String nom() {
+        return nom;
+    }
+
+    public int etoiles() {
+        return etoiles;
+    }
+
+    public Adresse adresse() {
+        return adresse;
+    }
+
+    public String url() {
+        return url;
+    }
 }
